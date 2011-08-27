e68640eec0fd38c7abcaa278d5568468f1084bd1
jrconlin <jconlin+git@mozilla.com>
2011-08-26 17:49:57 -0700

    Added frame code

--- notifserver/controllers/websocket.py
+++ notifserver/controllers/websocket.py
@@ -3,9 +3,16 @@
 # see http://tools.ietf.org/html/draft-ietf-hybi-thewebsocketprotocol-12
 IETF_VERSION = "DRAFT-12"
 
+
+from io import BytesIO
+
+import base64
+import hashlib
 import socket
 import os
 import struct
+import string
+
 
 class WebSocket:
 
@@ -52,13 +59,23 @@ class WebSocket:
             if websocket_key is None:
                 return ''
             base = "%s%s" % (websocket_key, self.WEBSOCKET_GUID)
+            key = base64.b64encode(hashlib.sha1(base))
+            return 'Sec-WebSocket-Accept: %s' % key
 
         def send_handshake(self, raw_request, additional_headers = None):
             request = self._parse_raw_request(raw_request)
+            #Check Sec-WebSocket-Protocol: contains service protocol
+            if 'protocol' in self.config:
+                if (self.config.get('protocol') not in
+                    request.get('sec-websocket-protocol','').lower()):
+                    raise WebSocketException("Unsupported protocol" +
+                       "requested: %s" % request.get('sec-websocket-protocol'))
             try:
+                # preflight headers.
                 handshake = ["HTTP/1.1 101 Web Socket Protocol Handshake",
                     "Upgrade: Websocket",
                     "Connection: Upgrade"]
+                # remote is a tuple of (address, socket)
                 handshake.append("WebSocket-Origin: %s:%s\r\n" % self.remote)
                 handshake.append("WebSocket-Location: %s\r\n" % \
                         self.config.get("local_address","ws://localhost"))
@@ -67,8 +84,76 @@ class WebSocket:
                 for h in additional_headers:
                     h.replace("\r",'').replace("\n",'')
                     if len(h):
-                        handshake += h + "\r\n"
-                handshake += "\r\n"
-                self.socket.send(handshake)
+                        handshake.append(h)
+                self.socket.send("%s\r\n\r\n" % "\r\n".join(handshake))
                 ack = self.socket.read(4)
-                #if ack != "\r\n\r\n"
+                if ack != "\r\n\r\n":
+                    raise WebSocketException("Incorrect ACK from client")
+            except Exception,e:
+                import pdb; pdb.set_trace()
+                raise
+
+        def mask(self, raw_data, mask):
+            io_stream = BytesIO(raw_data)
+            mbytes = [(mask & (0xFF << 0)) >> 0,
+                      (mask & (0xFF << 8)) >> 8,
+                      (mask & (0xFF << 16)) >> 16,
+                      (mask & (0xFF << 24)) >> 24]
+            output = BytesIO()
+            pos = 0
+            try:
+                char = io_stream.read(1)
+                while char:
+                    output.write(char ^ (mask & mbytes[pos % 4]))
+                    char = io_stream.read(1)
+            except IOError:
+                pass
+            return data
+
+        code2frame = {0x0: 'Continuation',
+                      0x1: 'Text',
+                      0x2: 'Binary',
+                      0x8: 'Close',
+                      0x9: 'Ping',
+                      0xA:'Pong',
+                      'Continuation': 0x0,
+                      'Text': 0x1,
+                      'Binary': 0x2,
+                      'Close': 0x8,
+                      'Ping': 0x9,
+                      'Pong': 0xA,
+                      }
+
+        def read_frame(self):
+            try:
+                frame = {}
+                byte = self.socket.read(1)
+                flags = (0xF0 & byte) >> 4
+                frame['is_finished'] = (0x8 & flags) > 0
+                frame['type'] = code2frame.get((byte and 0x0F),'Undefined')
+                byte = self.socket.read(1)
+                frame['is_masked'] = (0x80 & byte) > 0
+                byte = 0xF7 & byte
+                if byte < 125:
+                    frame['size'] = byte
+                elif byte == 126:
+                    frame['size'] = struct.unpack('!H', self.socket.read(2))
+                elif byte == 127:
+                    frame['size'] = struct.unpack('!Q', self.socket.read(8))
+                if frame['is_masked']:
+                    frame['mask'] = self.socket.read(4)
+                data = self.socket.read(frame['size'])
+                frame['data'] = self.mask(frame['mask'], data)
+                return frame
+            except IOError, e:
+                raise WebSocketException('Error reading data from socket')
+
+        def write_frame(self, frame):
+            if frame is None:
+                return
+            if not isinstance(frame, object):
+                raise WebSocketException('Frame should be a dictionary object')
+            data = BytesIO()
+            frame_size = len(frame['data'])
+            data.write('0x82')  #collapse this to Finished, BINARY type
+            frame_size =
