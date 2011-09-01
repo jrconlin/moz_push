29ba02f53f3376e9ea4a4adbf4f9d0339afeb1f1
jrconlin <jconlin+git@mozilla.com>
2011-08-31 17:03:51 -0700

    fixed up first run of client tests. Need moar.

---
+++ notifserver/tests/test_client_api.py
@@ -0,0 +1,141 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is the Mozilla Push Notifications Server. 
+#
+# The Initial Developer of the Original Code is
+# Mozilla Corporation.
+# Portions created by the Initial Developer are Copyright (C) 2011
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#  Shane da Silva <sdasilva@mozilla.com>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+import json
+import os
+import unittest
+
+from webtest import TestApp
+
+from notifserver.clientagent import make_client_agent
+from services.tests.support import TestEnv
+
+
+# API Version to use when making requests to Client Agent
+API = "1.0"
+
+
+class ClientAgentTest(unittest.TestCase):
+    """Test class for all Client Agent tests.
+
+    This handles the creation of an in-process Client Agent server to run
+    tests against.
+    
+    """
+
+    def setUp(self):
+        env = TestEnv(os.path.realpath(__file__))
+        self.config = env.config
+
+        self.appdir = env.topdir
+        self.app = TestApp(make_client_agent(self.config))
+
+    def tearDown(self):
+        pass
+
+    def set_credentials(self, username=None, password=None):
+        self.username = username
+        self.password = password
+
+    def create_queue(self):
+        res = self.app.post("/%s/new_queue" % API,
+            extra_environ={'REMOTE_USER': self.username})
+        assert res.status_int == 200
+
+        return json.loads(res.body)
+
+    def create_subscription(self, token):
+        res = self.app.post("/%s/new_subscription" % API,
+            json.dumps({'token': token}),
+            extra_environ={'REMOTE_USER': self.username})
+        assert res.status_int == 200
+
+    def remove_subscription(self, token):
+        res = self.app.post("/%s/remove_subscription" % API,
+            json.dumps({'token': token}),
+            extra_environ={'REMOTE_USER': self.username})
+
+    def send_broadcast(self, message):
+        test_msg = "this is a broadcast message"
+         
+        self.set_credentials("testuser", "mypassword")
+        res = self.app.post("/%s/send_broadcast" % API,
+            test_msg, 
+            extra_environ={'REMOTE_USER': self.username})
+
+        # TODO: Figure out a way to extract messages from queues in tests
+        # For now we assume all is well if "200 OK" returned
+
+    def test_create_queue(self):
+        self.set_credentials("testuser", "mypassword") 
+        queue_info = self.create_queue()
+        assert 'queue_id' in queue_info 
+        assert 'host' in queue_info 
+        assert 'port' in queue_info 
+
+        assert len(queue_info['queue_id']) > 0
+        assert len(queue_info['host']) > 0
+        assert queue_info['port']
+
+    def test_subscriptions(self):
+        self.set_credentials("testuser", "mypassword")
+        token = "TEST123456789"
+
+        # Can't delete subscription if it doesn't exist
+        try:
+            self.remove_subscription(token)
+            assert False
+        except:
+            pass
+
+        # Normal create and delete
+        self.create_subscription(token)
+        self.remove_subscription(token)
+
+        # Can't delete subscription if already deleted 
+        try:
+            self.remove_subscription(token)
+            assert False
+        except:
+            pass
+
+    def test_broadcasts(self):
+        self.set_credentials("testuser", "mypassword")
+
+        queue_info = self.create_queue()
+
+
